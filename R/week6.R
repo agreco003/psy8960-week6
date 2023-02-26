297a2cc9b74b84f19a091288f191cbc90837b7c0
agreco <greco031@umn.edu>
2023-02-26 02:05:08 -0600

    Project complete - final check

--- R/week6.R
+++ R/week6.R
@@ -19,8 +19,8 @@ citations_tbl <- enframe(citations_txt, name = "line", value = "cite") %>%
   mutate(year = str_match(cite, pattern = regex("\\((\\d\\d\\d\\d)\\)"))[,2]) %>%
   mutate(page_start = str_match(cite, pattern = regex("(\\d*)-\\d*\\.*\\s?\\.?$"))[,2]) %>%
   mutate(perf_ref = str_detect(cite, pattern = regex("performance", ignore_case = TRUE))) %>%
-  mutate(title = str_match(cite, pattern = regex("\\(\\d\\d\\d\\d\\)\\.\\s([^\\.]+)\\."))[,2]) %>%
-  mutate(first_author = str_match(cite, pattern = regex("([A-Z][a-z, ]+[\\s][A-Z]?[\\.]?[\\s]?[A-Z][.])[,]?[\\s]?"))[,2])
+  mutate(title = str_match(cite, pattern = regex("\\(\\d\\d\\d\\d\\)\\.\\s([^\\.\\?]+)\\."))[,2]) %>%
+  mutate(first_author = str_match(cite, pattern = regex("([A-Za-z ]+[,]?[\\s]?[A-Z]?[\\.]?[\\s]?[A-Z][.])[,]?[\\s]?"))[,2])
 
 sum(!is.na(citations_tbl$first_author))
   
@@ -35,5 +35,5 @@ sum(!is.na(citations_tbl$first_author))
 #str_match()[,2] works because the result is a dataframe that you can select a column from. Makes for lots of control
 #Spot checks for final row (next 3 lines):
 #trying to catch more authors not APA format =  mutate(first_author = str_match(cite, pattern = regex("([A-Z][a-z,]+[\\s]?[A-Z]?[\\.]?[,]?[\\s]?[A-Z]?[.]?[,]?)[\\s]?"))[,2])
-#print((citations_tbl$first_author))
-#print(!is.na(citations_tbl$first_author))
+print((citations_tbl$first_author))
+print(!is.na(citations_tbl$first_author))
