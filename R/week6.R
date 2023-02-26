15b5655b92bd333e8016c983f33e2377db54c3ab
agreco <greco031@umn.edu>
2023-02-25 20:17:46 -0600

    tibble created with correct columns. Testing notes added to end of file

--- R/week6.R
+++ R/week6.R
@@ -11,4 +11,13 @@ citations_txt <- str_subset(citations, pattern = ANY_CHAR)
 length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
 
-#list of encodings stri_enc_list(simplify = TRUE) ISO-8859-1 taken from class notes
+# Data Cleaning
+sample(citations_txt, 10)
+
+citations_tbl <- as_tibble(citations_txt, rownames = "line", coumn_name = "cite")
+
+# list of encodings stri_enc_list(simplify = TRUE) ISO-8859-1 taken from class notes
+# to test line 10
+#library(htmltools)
+#library(htmlwidgets)
+#str_view(citations, pattern = ANY_CHAR, match = FALSE)
